e403dee6016c529eb3c39e32da20915c7de8ba43
Eric Davenport <EricDavenport@pursuit.org>
2020-11-11 13:23:57 -0500

    Day 1 added

---
+++ HackerRankDay1/HackerRankDay1/main.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  HackerRankDay1
+//
+//  Created by Eric Davenport on 11/11/20.
+//
+//
+import Foundation
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declare second integer, double, and String variables.
+var int = Int()
+var double = Double()
+var string = String()
+
+// Read and save an integer, double, and String to your variables.
+int = Int(readLine()!) ?? 0
+double = Double(readLine()!) ?? 0.0
+string = readLine() ?? "no string"
+
+// Print the sum of both integer variables on a new line.
+print(int + i)
+
+// Print the sum of the double variables on a new line.
+print(double + d)
+
+// Concatenate and print the String variables on a new line
+print(s + string)
+// The 's' variable above should be printed first.
+
+
+// Mark: White board work
+/*
+print("Hello, World!")
+
+let string = 2
+
+var test = Int(readLine()!)
+
+print(string + test!)
+*/
